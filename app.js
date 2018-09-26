b804a47f657c865dacbe476088469d28bea681e7
John Herman Witham <john.h.witham@gmail.com>
2018-09-26 09:06:35 +0300

    Filter event. Loop through /w forEach and display by filtering searched input

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   // Clear task event
   clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -61,4 +63,19 @@ function removeTask(e) {
 // Clear Tasks
 function clearTasks() {
   taskList.innerHTML = '';
+}
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach
+  (function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
